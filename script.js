937e7999404349157e00e089d7a70c4a6a3484e0
stevenvanelk <srvanelk@gmail.com>
2025-03-02 10:04:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ function toggleMenu() {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".menu-toggle").addEventListener("click", toggleMenu);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("year").textContent = new Date().getFullYear();
 });
 
