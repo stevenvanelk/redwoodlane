407082c22d3abd45c6342c9fa23f9efa5115afd9
stevenvanelk <srvanelk@gmail.com>
2025-03-01 20:14:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function toggleMenu() {
     const nav = document.querySelector(".nav-links");
     nav.classList.toggle("nav-active");
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("year").textContent = new Date().getFullYear();
+});
+
